dc79a0f9a85ae640491cae1e474ebd2bee08d67f
末心 <59173630+Moxin1044@users.noreply.github.com>
2022-12-31 08:07:32 +0800

    update

---
+++ README_ZH.md
@@ -0,0 +1,23 @@
+# 调用方法
+
+## 配置文件
+
+配置文件位于根目录下的`config.json`
+
+![](./img/README_ZH/image-20221231074753562.png)
+
+其中，email填写您的Fofa注册email，key填写fofa的api-key。
+
+proxy默认为空，如需代理，可对其进行填写。
+
+## 检查Fofa配置
+
+![](./img/README_ZH/image-20221231074916988.png)
+
+```python
+clients = Client()
+print(clients.check_fofa_config())
+```
+
+只需要按照上述方法配置，即可检查配置信息。会在控制台输出你当前的`email`、`key`和`proxy`。
+
---
+++ fofa/client.py
@@ -0,0 +1,32 @@
+import json
+import requests
+
+
+class Client:
+    def __init__(self):
+        self.username = None
+        self.email_check = None
+        # load config.json
+        with open('../config.json', 'r') as f:
+            config = json.load(f)
+        self.email =config['email']
+        self.key = config['key']
+        self.proxy = config['proxy']
+        self.get_userinfo()
+
+    def check_fofa_config(self):
+        return f"Email:{self.email} Key:{self.key} Proxy:{self.proxy}"
+
+    def get_userinfo(self):
+        url = f"https://fofa.info/api/v1/info/my?email={self.email}&key={self.key}"
+        response = requests.get(url).json()
+        if response['error']:
+            return response['errmsg']
+        else:
+            self.email_check = response['email']
+            self.username = response['username']
+            return self
+
+
+clients = Client()
+print(clients.email_check)
