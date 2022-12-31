50c6eff85383ddb43ff27ed92093361283d711b2
Abyssun <2025353414@qq.com>
2022-12-31 10:48:26 +0800

    Update client.py

--- fofa/client.py
+++ fofa/client.py
@@ -64,7 +64,11 @@ class Client:
         return response
 
     def search_host(self,host, detail=False):
-        url = f""
+        if detail is None:
+            detail = ['title']
+        detail = 
+        host = 
+        url = f"https://fofa.info/api/v1/host/{host}?email={self.email}&key={self.key}"
         response = fofa.operation.send_get_json(url)
         return response
 
